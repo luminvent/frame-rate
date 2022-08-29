d64d2ef2dffa7d3396a4f5f5071332fcd4fd3ea9
Marc-Antoine Arnaud <marc-antoine.arnaud@luminvent.com>
2022-08-29 05:28:55 +0200

    change enum prefix from Fr to _

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,16 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum FrameRate {
-  Fr24,
-  Fr25,
-  Fr30,
-  Fr50,
-  Fr60,
-  Fr120,
-  Fr23_97,
-  Fr24_97,
-  Fr29_97,
-  Fr59_94,
+  _24_00,
+  _25_00,
+  _30_00,
+  _50_00,
+  _60_00,
+  _120_00,
+  _23_97,
+  _24_97,
+  _29_97,
+  _59_94,
   FrCustom(Ratio<u32>),
 }
 
@@ -32,16 +32,16 @@ impl From<&FrameRate> for f64 {
 impl From<FrameRate> for Ratio<u32> {
   fn from(frame_rate: FrameRate) -> Self {
     match frame_rate {
-      FrameRate::Fr24 => Self::from_integer(24),
-      FrameRate::Fr25 => Self::from_integer(25),
-      FrameRate::Fr30 => Self::from_integer(30),
-      FrameRate::Fr50 => Self::from_integer(50),
-      FrameRate::Fr60 => Self::from_integer(60),
-      FrameRate::Fr120 => Self::from_integer(120),
-      FrameRate::Fr23_97 => Self::new(24000, 1001),
-      FrameRate::Fr24_97 => Self::new(25000, 1001),
-      FrameRate::Fr29_97 => Self::new(30000, 1001),
-      FrameRate::Fr59_94 => Self::new(60000, 1001),
+      FrameRate::_24_00 => Self::from_integer(24),
+      FrameRate::_25_00 => Self::from_integer(25),
+      FrameRate::_30_00 => Self::from_integer(30),
+      FrameRate::_50_00 => Self::from_integer(50),
+      FrameRate::_60_00 => Self::from_integer(60),
+      FrameRate::_120_00 => Self::from_integer(120),
+      FrameRate::_23_97 => Self::new(24000, 1001),
+      FrameRate::_24_97 => Self::new(25000, 1001),
+      FrameRate::_29_97 => Self::new(30000, 1001),
+      FrameRate::_59_94 => Self::new(60000, 1001),
       FrameRate::FrCustom(rational) => rational,
     }
   }
@@ -50,16 +50,16 @@ impl From<FrameRate> for Ratio<u32> {
 impl From<Ratio<u32>> for FrameRate {
   fn from(rational: Ratio<u32>) -> Self {
     match (rational.numer(), rational.denom()) {
-      (24, 1) => Self::Fr24,
-      (25, 1) => Self::Fr25,
-      (30, 1) => Self::Fr30,
-      (50, 1) => Self::Fr50,
-      (60, 1) => Self::Fr60,
-      (120, 1) => Self::Fr120,
-      (24000, 1001) => Self::Fr23_97,
-      (25000, 1001) => Self::Fr24_97,
-      (30000, 1001) => Self::Fr29_97,
-      (60000, 1001) => Self::Fr59_94,
+      (24, 1) => Self::_24_00,
+      (25, 1) => Self::_25_00,
+      (30, 1) => Self::_30_00,
+      (50, 1) => Self::_50_00,
+      (60, 1) => Self::_60_00,
+      (120, 1) => Self::_120_00,
+      (24000, 1001) => Self::_23_97,
+      (25000, 1001) => Self::_24_97,
+      (30000, 1001) => Self::_29_97,
+      (60000, 1001) => Self::_59_94,
       _ => Self::FrCustom(rational),
     }
   }
@@ -112,16 +112,16 @@ mod tests {
 
   #[test]
   fn rational_from_frame_rate() {
-    assert_eq!(Ratio::from(FrameRate::Fr24), Ratio::from_integer(24));
-    assert_eq!(Ratio::from(FrameRate::Fr25), Ratio::from_integer(25));
-    assert_eq!(Ratio::from(FrameRate::Fr30), Ratio::from_integer(30));
-    assert_eq!(Ratio::from(FrameRate::Fr50), Ratio::from_integer(50));
-    assert_eq!(Ratio::from(FrameRate::Fr60), Ratio::from_integer(60));
-    assert_eq!(Ratio::from(FrameRate::Fr120), Ratio::from_integer(120));
-    assert_eq!(Ratio::from(FrameRate::Fr23_97), Ratio::new(24000, 1001));
-    assert_eq!(Ratio::from(FrameRate::Fr24_97), Ratio::new(25000, 1001));
-    assert_eq!(Ratio::from(FrameRate::Fr29_97), Ratio::new(30000, 1001));
-    assert_eq!(Ratio::from(FrameRate::Fr59_94), Ratio::new(60000, 1001));
+    assert_eq!(Ratio::from(FrameRate::_24_00), Ratio::from_integer(24));
+    assert_eq!(Ratio::from(FrameRate::_25_00), Ratio::from_integer(25));
+    assert_eq!(Ratio::from(FrameRate::_30_00), Ratio::from_integer(30));
+    assert_eq!(Ratio::from(FrameRate::_50_00), Ratio::from_integer(50));
+    assert_eq!(Ratio::from(FrameRate::_60_00), Ratio::from_integer(60));
+    assert_eq!(Ratio::from(FrameRate::_120_00), Ratio::from_integer(120));
+    assert_eq!(Ratio::from(FrameRate::_23_97), Ratio::new(24000, 1001));
+    assert_eq!(Ratio::from(FrameRate::_24_97), Ratio::new(25000, 1001));
+    assert_eq!(Ratio::from(FrameRate::_29_97), Ratio::new(30000, 1001));
+    assert_eq!(Ratio::from(FrameRate::_59_94), Ratio::new(60000, 1001));
     let rational_2_3 = Ratio::new(2, 3);
     let rational_6_9 = Ratio::new(6, 9);
     assert_eq!(Ratio::from(FrameRate::FrCustom(rational_2_3)), rational_2_3);
@@ -130,16 +130,19 @@ mod tests {
 
   #[test]
   fn frame_rate_from_rational() {
-    assert_eq!(FrameRate::from(Ratio::from_integer(24)), FrameRate::Fr24);
-    assert_eq!(FrameRate::from(Ratio::from_integer(25)), FrameRate::Fr25);
-    assert_eq!(FrameRate::from(Ratio::from_integer(30)), FrameRate::Fr30);
-    assert_eq!(FrameRate::from(Ratio::from_integer(50)), FrameRate::Fr50);
-    assert_eq!(FrameRate::from(Ratio::from_integer(60)), FrameRate::Fr60);
-    assert_eq!(FrameRate::from(Ratio::from_integer(120)), FrameRate::Fr120);
-    assert_eq!(FrameRate::from(Ratio::new(24000, 1001)), FrameRate::Fr23_97);
-    assert_eq!(FrameRate::from(Ratio::new(25000, 1001)), FrameRate::Fr24_97);
-    assert_eq!(FrameRate::from(Ratio::new(30000, 1001)), FrameRate::Fr29_97);
-    assert_eq!(FrameRate::from(Ratio::new(60000, 1001)), FrameRate::Fr59_94);
+    assert_eq!(FrameRate::from(Ratio::from_integer(24)), FrameRate::_24_00);
+    assert_eq!(FrameRate::from(Ratio::from_integer(25)), FrameRate::_25_00);
+    assert_eq!(FrameRate::from(Ratio::from_integer(30)), FrameRate::_30_00);
+    assert_eq!(FrameRate::from(Ratio::from_integer(50)), FrameRate::_50_00);
+    assert_eq!(FrameRate::from(Ratio::from_integer(60)), FrameRate::_60_00);
+    assert_eq!(
+      FrameRate::from(Ratio::from_integer(120)),
+      FrameRate::_120_00
+    );
+    assert_eq!(FrameRate::from(Ratio::new(24000, 1001)), FrameRate::_23_97);
+    assert_eq!(FrameRate::from(Ratio::new(25000, 1001)), FrameRate::_24_97);
+    assert_eq!(FrameRate::from(Ratio::new(30000, 1001)), FrameRate::_29_97);
+    assert_eq!(FrameRate::from(Ratio::new(60000, 1001)), FrameRate::_59_94);
     let rational_2_3 = Ratio::new(2, 3);
     let rational_6_9 = Ratio::new(6, 9);
     assert_eq!(
@@ -150,76 +153,76 @@ mod tests {
       FrameRate::from(rational_2_3),
       FrameRate::FrCustom(rational_6_9)
     );
-    assert_eq!(FrameRate::from(Ratio::new(200, 4)), FrameRate::Fr50);
+    assert_eq!(FrameRate::from(Ratio::new(200, 4)), FrameRate::_50_00);
   }
 
   #[test]
   fn serialize() {
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr24).unwrap(),
+      serde_json::to_value(&FrameRate::_24_00).unwrap(),
       serde_json::json!({
         "num": 24,
         "den": 1
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr25).unwrap(),
+      serde_json::to_value(&FrameRate::_25_00).unwrap(),
       serde_json::json!({
         "num": 25,
         "den": 1
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr30).unwrap(),
+      serde_json::to_value(&FrameRate::_30_00).unwrap(),
       serde_json::json!({
         "num": 30,
         "den": 1
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr50).unwrap(),
+      serde_json::to_value(&FrameRate::_50_00).unwrap(),
       serde_json::json!({
         "num": 50,
         "den": 1
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr60).unwrap(),
+      serde_json::to_value(&FrameRate::_60_00).unwrap(),
       serde_json::json!({
         "num": 60,
         "den": 1
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr120).unwrap(),
+      serde_json::to_value(&FrameRate::_120_00).unwrap(),
       serde_json::json!({
         "num": 120,
         "den": 1
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr23_97).unwrap(),
+      serde_json::to_value(&FrameRate::_23_97).unwrap(),
       serde_json::json!({
         "num": 24000,
         "den": 1001
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr24_97).unwrap(),
+      serde_json::to_value(&FrameRate::_24_97).unwrap(),
       serde_json::json!({
         "num": 25000,
         "den": 1001
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr29_97).unwrap(),
+      serde_json::to_value(&FrameRate::_29_97).unwrap(),
       serde_json::json!({
         "num": 30000,
         "den": 1001
       })
     );
     assert_eq!(
-      serde_json::to_value(&FrameRate::Fr59_94).unwrap(),
+      serde_json::to_value(&FrameRate::_59_94).unwrap(),
       serde_json::json!({
         "num": 60000,
         "den": 1001
@@ -249,7 +252,7 @@ mod tests {
         "den": 1
       }))
       .unwrap(),
-      FrameRate::Fr24
+      FrameRate::_24_00
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -257,7 +260,7 @@ mod tests {
         "den": 1
       }))
       .unwrap(),
-      FrameRate::Fr25
+      FrameRate::_25_00
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -265,7 +268,7 @@ mod tests {
         "den": 1
       }))
       .unwrap(),
-      FrameRate::Fr30
+      FrameRate::_30_00
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -273,7 +276,7 @@ mod tests {
         "den": 1
       }))
       .unwrap(),
-      FrameRate::Fr50
+      FrameRate::_50_00
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -281,7 +284,7 @@ mod tests {
         "den": 1
       }))
       .unwrap(),
-      FrameRate::Fr60
+      FrameRate::_60_00
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -289,7 +292,7 @@ mod tests {
         "den": 1
       }))
       .unwrap(),
-      FrameRate::Fr120
+      FrameRate::_120_00
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -297,7 +300,7 @@ mod tests {
         "den": 1001
       }))
       .unwrap(),
-      FrameRate::Fr23_97
+      FrameRate::_23_97
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -305,7 +308,7 @@ mod tests {
         "den": 1001
       }))
       .unwrap(),
-      FrameRate::Fr24_97
+      FrameRate::_24_97
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -313,7 +316,7 @@ mod tests {
         "den": 1001
       }))
       .unwrap(),
-      FrameRate::Fr29_97
+      FrameRate::_29_97
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -321,7 +324,7 @@ mod tests {
         "den": 1001
       }))
       .unwrap(),
-      FrameRate::Fr59_94
+      FrameRate::_59_94
     );
     assert_eq!(
       serde_json::from_value::<FrameRate>(serde_json::json!({
@@ -345,7 +348,7 @@ mod tests {
         "den": 4
       }))
       .unwrap(),
-      FrameRate::Fr50
+      FrameRate::_50_00
     );
   }
 }
